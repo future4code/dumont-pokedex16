edd4a46c4955d69bdbaaeac90e9eff2f1120c563
Kelly Knoblauch <kellyknoblauch@MacBook-Pro-de-Kelly.local>
2020-11-23 14:52:48 -0300

    terminando componentizacao

---
+++ pokedex/src/pages/homePage/homePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { useHistory } from 'react-router-dom;'
+
+
+const HomePage = () => {
+  
+  return (
+
+    <div>
+        Home
+    </div>
+    
+ 
+  )
+}
+
+export default HomePage
--- pokedex/src/routes/Router.js
+++ pokedex/src/routes/Router.js
@@ -1,2 +1,30 @@
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route} from "react-router-dom";
 import React from 'react';
+import HomePage from "../pages/HomePage/HomePage";
+import PokedexPage from "../pages/PokedexPage/PokedexPage";
+import DetailsPage from "../pages/DetailsPage/DetailsPage";
+
+
+
+function Router() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/pokedex">
+            <PokedexPage />
+          </Route>
+          <Route exact path="/pokedex/details/:id">
+            <DetailsPage />
+          </Route>
+          <Route>
+             <div>404: Página não encontrada</div>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+  
+  export default Router;
